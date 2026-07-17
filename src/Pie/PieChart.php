de9b04dd3db35071b6b0853a95e77c9878e998fe
Andy Thomas <git@andythom.as>
2026-07-17 18:07:43 +0100

    Pie Chart: Single Slice Rendering Fix
    - Make a filtered copy of slices to use for rendering & fix single/multiple slice rendering.

--- src/Pie/PieChart.php
+++ src/Pie/PieChart.php
@@ -55,39 +55,13 @@ class PieChart
         $currentAngle = 0;
         $svg = '';
 
-        foreach ($this->slices as $slice) {
-            if ($slice->value <= 0) {
-                continue;
-            }
+        $renderableSlices = array_filter(
+            $this->slices,
+            fn($slice) => $slice->value > 0
+        );
 
-            $sliceAngle = ($slice->value / $total) * 360;
-            $currentAngle += $sliceAngle;
-            $largeArcFlag = $sliceAngle > 180 ? 1 : 0;
-
-            $midAngle = deg2rad($currentAngle - $sliceAngle / 2);
-            $explodeX = $slice->explodeDistance * cos($midAngle);
-            $explodeY = $slice->explodeDistance * sin($midAngle);
-
-            $adjustedCx = $cx + $explodeX;
-            $adjustedCy = $cy + $explodeY;
-
-            $x1 = $adjustedCx + $radius * cos(deg2rad($currentAngle - $sliceAngle));
-            $y1 = $adjustedCy + $radius * sin(deg2rad($currentAngle - $sliceAngle));
-            $x2 = $adjustedCx + $radius * cos(deg2rad($currentAngle));
-            $y2 = $adjustedCy + $radius * sin(deg2rad($currentAngle));
-
-            $pathData = "M $adjustedCx,$adjustedCy L $x1,$y1 A $radius,$radius 0 $largeArcFlag,1 $x2,$y2 Z";
-
-            $labelX = $adjustedCx + ($radius / 1.5) * cos($midAngle);
-            $labelY = $adjustedCy + ($radius / 1.5) * sin($midAngle);
-
-            $percentage = round(($slice->value / $total) * 100, 2);
-
-            $svg .= $slice->render($this, $pathData, $labelX, $labelY, $percentage);
-        }
-
-        if (count($this->slices) === 1) {
-            $slice = $this->slices[0];
+        if (count($renderableSlices) === 1) {
+            $slice = array_shift($renderableSlices);
             $pathData = "M $cx,$cy m -$radius,0 a $radius,$radius 0 1,0 ".(2 * $radius).",0 a $radius,$radius 0 1,0 -".(2 * $radius).',0 Z';
 
             $labelX = $cx;
@@ -96,6 +70,37 @@ class PieChart
             $percentage = 100;
 
             $svg = $slice->render($this, $pathData, $labelX, $labelY, $percentage);
+        } else {
+            foreach ($renderableSlices as $slice) {
+                if ($slice->value <= 0) {
+                    continue;
+                }
+
+                $sliceAngle = ($slice->value / $total) * 360;
+                $currentAngle += $sliceAngle;
+                $largeArcFlag = $sliceAngle > 180 ? 1 : 0;
+
+                $midAngle = deg2rad($currentAngle - $sliceAngle / 2);
+                $explodeX = $slice->explodeDistance * cos($midAngle);
+                $explodeY = $slice->explodeDistance * sin($midAngle);
+
+                $adjustedCx = $cx + $explodeX;
+                $adjustedCy = $cy + $explodeY;
+
+                $x1 = $adjustedCx + $radius * cos(deg2rad($currentAngle - $sliceAngle));
+                $y1 = $adjustedCy + $radius * sin(deg2rad($currentAngle - $sliceAngle));
+                $x2 = $adjustedCx + $radius * cos(deg2rad($currentAngle));
+                $y2 = $adjustedCy + $radius * sin(deg2rad($currentAngle));
+
+                $pathData = "M $adjustedCx,$adjustedCy L $x1,$y1 A $radius,$radius 0 $largeArcFlag,1 $x2,$y2 Z";
+
+                $labelX = $adjustedCx + ($radius / 1.5) * cos($midAngle);
+                $labelY = $adjustedCy + ($radius / 1.5) * sin($midAngle);
+
+                $percentage = round(($slice->value / $total) * 100, 2);
+
+                $svg .= $slice->render($this, $pathData, $labelX, $labelY, $percentage);
+            }
         }
 
         return $svg;
